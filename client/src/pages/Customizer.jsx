602b98be3e46723e5b39a6f760b14d9720da9b6d
Zaid-maker <pzhafeez@gmail.com>
2023-04-26 15:38:20 +0500

    :bug: update production url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -63,15 +63,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("https://localhost:3000/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://ai-3d-website.zaidmaker.repl.co/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
